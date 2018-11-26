61cc59f2c9fd35857704bf5069b3cf2b8214831b
Jean Dobler <jedobler@gmail.com>
2018-11-26 01:32:24 -0200

    Showfront Activity

--- showfront/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ showfront/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,17 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTvJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView mTvJoke = findViewById(R.id.tv_joke);
         setContentView(R.layout.activity_main);
     }
 }
