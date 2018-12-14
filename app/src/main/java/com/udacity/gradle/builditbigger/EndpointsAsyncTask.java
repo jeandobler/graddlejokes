6b9fbe517c99aa5aa2914c6ee2776795558e390f
Jean Dobler <jedobler@gmail.com>
2018-12-13 22:24:38 -0200

    Voltando para Local

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -22,7 +22,7 @@ class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
     protected String doInBackground(Context... params) {
         if (jokeEndpointService == null) {  // Only do this once
             JokeEndpoint.Builder builder = new JokeEndpoint.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                    .setRootUrl("https://api-graddle.appspot.com/_ah/api/")
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
